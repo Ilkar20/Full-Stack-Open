fd2ce08a2333d16fa2efe2761e2a9e504119a031
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2025-09-15 20:20:08 +0300

    solved exercise 1.3-1.5

---
+++ part1/Exercises 1.3.-1.5/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course.name}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            {props.course.parts.map((part, index) => (
+                <p key={index}>{part.name} {part.exercises}</p>
+            ))}
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+    )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+export default App
