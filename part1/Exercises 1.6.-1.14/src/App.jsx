ba5c62fc50ab5cb7c39b028dc0754147c790d64e
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2025-09-16 13:39:09 +0300

    1.10: unicafe step 5

--- part1/Exercises 1.6.-1.14/src/App.jsx
+++ part1/Exercises 1.6.-1.14/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <div>
+      <p>{props.text} {props.value} {props.text === "positive" ? "%" : ""}</p>
+    </div>
+    
+  )
+}
+
 const Statistics = (props) => {
   if (props.all === 0) {
     return <p>No feedback given</p>
@@ -8,15 +17,16 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.all}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.all} />
+      <StatisticLine text="average" value={props.average} />
+      <StatisticLine text="positive" value={props.positive} />
     </div>
   )
 }
+
 
 const App = () => {
   // save clicks of each button to its own state
