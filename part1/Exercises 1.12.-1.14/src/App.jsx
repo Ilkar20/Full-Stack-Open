e687fd1789e7d3157e4c60b4e210705ccb1966f4
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2025-09-16 20:39:19 +0300

    1.13*: anecdotes step 2

--- part1/Exercises 1.12.-1.14/src/App.jsx
+++ part1/Exercises 1.12.-1.14/src/App.jsx
@@ -13,16 +13,25 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const handleNextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>Vote</button>
       <button onClick={handleNextAnecdote}>Next Anecdote</button>
     </div>
   )
