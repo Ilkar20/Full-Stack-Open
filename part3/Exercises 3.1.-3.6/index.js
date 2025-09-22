e106656ae5faeacd0cf40ba29d80f90778edc8fd
Yilikaer Yihamujiang <ilkar.elham@gmail.com>
2025-09-22 10:23:38 +0300

    3.2: Phonebook backend step 2

--- part3/Exercises 3.1.-3.6/index.js
+++ part3/Exercises 3.1.-3.6/index.js
@@ -6,6 +6,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+  const personCoiunt = persons.length;
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${personCoiunt} people</p><p>${date}</p>`);
+});
+ 
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
