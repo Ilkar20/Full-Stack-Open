618e558ef0941dc92cc9dd8fb2d56aac7a84a883
Yilikaer Yihamujiang <ilkar.elham@gmail.com>
2025-10-06 12:24:21 +0300

    added some code

---
+++ part3/Exercise 3.19-3.21/backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors')
+const morganLogger = require('./middlewares/morganLogger')
+const errorHandler = require('./middlewares/errorHandler')
+const personsRouter = require('./routes/persons')
+const Person = require('./models/person');
+
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(morganLogger)
+
+// Routes
+app.use('/api/persons', personsRouter)
+
+// Info route
+app.get('/info', (req, res, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      const info = `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`
+      res.send(info)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+app.use((req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+} )
+
+// Error handling middleware
+app.use(errorHandler)
+
+module.exports = app
---
+++ part3/Exercise 3.19-3.21/frontend/src/components/PersonForm.jsx
@@ -0,0 +1,14 @@
+const personForm = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) => (
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+)
+export default personForm
