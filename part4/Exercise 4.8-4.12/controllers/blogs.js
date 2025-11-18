4cadf2df3e2ed824f41f237834f60eceb39bef24
Yilikaer Yihamujiang <ilkar.elham@gmail.com>
2025-11-18 13:57:36 +0200

    added exercise 4.8

---
+++ part4/Exercise 4.8-4.12/controllers/blogs.js
@@ -0,0 +1,19 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(savedBlog => {
+      response.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogRouter
---
+++ part4/Exercise 4.8-4.12/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const { test, describe, beforeEach, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const assert = require('assert')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual((await helper.blogsInDb()).length, helper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
