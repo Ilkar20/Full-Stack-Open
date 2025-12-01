02c278486836560ea94f5ce91c91e5ef228d735b
Yilikaer Yihamujiang <ilkar.elham@gmail.com>
2025-12-01 13:28:10 +0200

    solved: 4.15: Blog List Expansion, step 3

---
+++ part4/Exercise 4.15-4.23/controllers/blogs.js
@@ -0,0 +1,72 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const { title , author, url, likes } = request.body
+
+  if (!title || !url) {
+    return response.status(400).json({ error: 'title or url is missing' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes || 0
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
+  try {
+    const deleteBlog = await Blog.findByIdAndDelete(request.params.id)
+
+    if (deleteBlog) {
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      blog,
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+module.exports = blogRouter
