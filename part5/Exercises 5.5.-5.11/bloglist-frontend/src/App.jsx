f506d22e40642e6cc803fa6542970fa2c3212c24
Yilikaer Yihamujiang <ilkar.elham@gmail.com>
2026-01-22 14:48:57 +0200

    Solved: 5.5 Blog List Frontend

---
+++ part5/Exercises 5.5.-5.11/bloglist-frontend/src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState({ message: null, type: null })
+  
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      console.log('Restoring logged in user from local storage:', user)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const showNotification = (message, type='success') => {
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification({ message: null, type: null })
+    }, 5000)
+  }
+
+  const handleLogin = async (credentials) => {
+    try {
+      const user = await loginService.login(credentials)
+
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      showNotification(`Welcome back, ${user.name}!`, 'success')
+    } catch (error) {
+      console.error('Login failed:', error)
+      alert('Invalid credentials')
+      showNotification('Wrong username or password', 'error')
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    blogService.setToken(null)
+    setUser(null)
+  }
+
+  const addBlog = async (blogObject) => {
+    try {
+      const createdBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(createdBlog))
+      showNotification(`a new blog ${createdBlog.title} by ${createdBlog.author} added`, 'success')
+    } catch (error) {
+      console.error('Failed to create blog:', error)
+      showNotification('Failed to create blog', 'error')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification message = {notification.message} type={notification.type} />
+        <LoginForm onLogin={handleLogin} />
+      </div>
+    )
+  }
+  
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification message = {notification.message} type={notification.type} />
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <Togglable buttonLabel="create new blog">
+        <h2>Create new</h2>
+        <BlogForm onBlogCreated={addBlog} />
+      </Togglable>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
